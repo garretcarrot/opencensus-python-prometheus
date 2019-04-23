d5e63a153b0950ac09d7e3c12b9d0f3bef8d48f8
Garret Thompson <gthompson@rsglab.com>
2019-04-23 11:52:28 -0400

    Add things to measure

--- repl.py
+++ repl.py
@@ -2,6 +2,18 @@
 
 import sys
 
+from opencensus.stats import aggregation
+from opencensus.stats import measure
+from opencensus.stats import stats
+from opencensus.stats import view
+from opencensus.tags import tag_key
+from opencensus.tags import tag_map
+from opencensus.tags import tag_value
+
+# Make the measures
+m_latency_ms = measure.MeasureFloat("repl_latency", "The latency in millisecnds per REPL loop", "ms")
+m_line_lengths = measure.MeasureInt("repl_line_lengths", "The distributes of line lengths", "By")
+
 def main():
     while True:
         line = sys.stdin.readline()
