11557cb74e81c00a7ede506bce1d41b560f4e38f
Garret Thompson <gthompson@rsglab.com>
2019-04-23 12:01:41 -0400

    Measure the things

--- repl.py
+++ repl.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import sys
+import time
 
 from opencensus.stats import aggregation
 from opencensus.stats import measure
@@ -14,10 +15,27 @@ from opencensus.tags import tag_value
 m_latency_ms = measure.MeasureFloat("repl_latency", "The latency in millisecnds per REPL loop", "ms")
 m_line_lengths = measure.MeasureInt("repl_line_lengths", "The distributes of line lengths", "By")
 
+# Make the thing that records the measurements
+stats_recorder = stats.Stats().stats_recorder
+
 def main():
     while True:
-        line = sys.stdin.readline()
-        print(line.upper())
+        readEvalPrint()
+
+def readEvalPrint():
+    line = sys.stdin.readline()
+    start = time.time()
+    print(line.upper())
+
+    # Make a thing to store measurements
+    measure_map = stats_recorder.new_measurement_map()
+
+    # Record the duration
+    end_ms = (time.time() - start) * 1000.0
+    measure_map.measure_float_put(m_latency_ms, end_ms)
+
+    # Record the line length
+    measure_map.measure_int_put(m_line_lengths, len(line))
 
 if __name__ == "__main__":
     main()
